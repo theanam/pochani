65274a297c06f735a4a085641f4975e164cd5317
Anam Ahmed <anam.ahmed.a@gmail.com>
2015-11-09 17:43:49 +0600

    Video not available with file:// protocol

--- base_template/js/toolbox.js
+++ base_template/js/toolbox.js
@@ -6,15 +6,22 @@ $('.pin').on('click',function(){
 $('.annotation').on('click',function(){
 	toggleAnnotation();
 });
-$('.video').on('click',function(){
-	/*The functions are in video.js*/
-	if(videoPlaying){
-		turnOffVideo();
-	}
-	else{
-		turnOnVideo();
-	}
-});
+//hide video if the file is opened locally
+if(document.URL.match("file:")){
+	alert("Webcam Video will not work if the file is opened without a server.")
+	$('.video').hide();
+}
+else{
+	$('.video').on('click',function(){
+		/*The functions are in video.js*/
+		if(videoPlaying){
+			turnOffVideo();
+		}
+		else{
+			turnOnVideo();
+		}
+	});
+}
 $('.colors').on('click','.button',function(){
 	ctx.strokeStyle=this.style.backgroundColor; /*ctx is from drawing.js*/
     //remove existing reference
